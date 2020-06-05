5d978baf5422e8408cc2927ae5e6142b6024e6f0
Alimova Ekaterina <49318955+AlimovaKatrin@users.noreply.github.com>
2020-06-05 11:53:12 +0300

    Add files via upload

---
+++ reader.cpp
@@ -0,0 +1,11 @@
+// #include <iostream>
+// #include <fstream>
+
+// int main(int ard, const char *argv[])
+// {
+//     std::ofstream file;
+//     file.open("text.txt");
+//     file.close()
+//     std::cin.get();
+//     return 0;
+// }
