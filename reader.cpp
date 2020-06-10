5fad97e4e417fc38d08068c536c0d8239a967f2c
Alimova Ekaterina <49318955+AlimovaKatrin@users.noreply.github.com>
2020-06-10 10:24:10 +0300

    Update reader.cpp

--- reader.cpp
+++ reader.cpp
@@ -13,27 +13,4 @@ int main()
     cout << text;
   }
   File.close();
-  // return 0;
 }
-
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-// using namespace std;
-
-// int main () {
-//   // Create a text string, which is used to output the text file
-//   string myText;
-
-//   // Read from the text file
-//   ifstream MyReadFile("text.txt");
-
-//   // Use a while loop together with the getline() function to read the file line by line
-//   while (getline (MyReadFile, myText)) {
-//     // Output the text from the file
-//     cout << myText;
-//   }
-
-//   // Close the file
-//   MyReadFile.close();
-// }
